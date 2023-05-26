ac6bd18de854d961abc65007533cebe3f0f04da2
guowuchen <100406756+guowuchen@users.noreply.github.com>
2023-05-26 21:45:56 +0800

    Update RifGenerator.hh

--- apps/rosetta/riflib/rif/RifGenerator.hh
+++ apps/rosetta/riflib/rif/RifGenerator.hh
@@ -42,8 +42,8 @@ namespace rif {
 
 using ::devel::scheme::shared_ptr;
 
-struct RifAccumulator {
-	virtual ~RifAccumulator(){}
+struct RifAccumulator { //声明一个结构体RifAccumulator
+	virtual ~RifAccumulator(){} //RifAccumulator的析构函数
 	virtual void insert( EigenXform const & x, float score, int rot, int sat1=-1, int sat2=-1, bool force=false, bool single_thread=false) = 0;
 	virtual void report( std::ostream & out ) const = 0;
 	virtual void checkpoint( std::ostream & out, bool force_override=false ) = 0;
@@ -57,27 +57,27 @@ struct RifAccumulator {
 	virtual std::set<size_t> get_sats_of_this_irot( devel::scheme::EigenXform const & x, int irot ) const = 0;
 	virtual bool initialize_with_rif( shared_ptr<RifBase> & rif ) = 0;
 };
-typedef shared_ptr<RifAccumulator> RifAccumulatorP;
+typedef shared_ptr<RifAccumulator> RifAccumulatorP; //将RifAccumulator类型的指针取别名为RifAccumulator
 
-struct RifGenParams {
-  	core::pose::PoseOP             target = nullptr;
-	std::string                    target_tag;
-	std::string                    output_prefix="./default_";
+struct RifGenParams { //声明一个结构体RifGenParams
+  	core::pose::PoseOP             target = nullptr; //core::pose::PoseOP类型的指针
+	std::string                    target_tag; //字符串target_tag
+	std::string                    output_prefix="./default_"; //字符串输出后缀
 	// set the fine-tuning file here, so it can control the rifgen processes of polar, apolar and hotspots.
   std::string																			 tuning_file="";
 	utility::vector1<int>          target_res;
 	//shared_ptr<RotamerIndex const> rot_index_p = nullptr;
-	shared_ptr<RotamerIndex> rot_index_p = nullptr;
-	std::vector<std::string>       cache_data_path;
-	std::vector< VoxelArray* >     field_by_atype;
+	shared_ptr<RotamerIndex> rot_index_p = nullptr; //RotamerIndex类型的指针rot_index_p
+	std::vector<std::string>       cache_data_path; //存放缓存数据路径的容器vector
+	std::vector< VoxelArray* >     field_by_atype; //存放指针VoxelArray*的vector
 	HBRayOpts                      hbopt;
 #ifdef USEGRIDSCORE
 	shared_ptr<protocols::ligand_docking::ga_ligand_dock::GridScorer> grid_scorer;
 #endif
 	// the only reason this is a pointer is to avoid including a bunch of stuff this high up
-	shared_ptr<RifScoreRotamerVsTarget> rot_tgt_scorer;
+	shared_ptr<RifScoreRotamerVsTarget> rot_tgt_scorer; //RifScoreRotamerVsTarget类型的指针rot_tgt_scorer
 };
-typedef shared_ptr<RifGenParams> RifGenParamsP;
+typedef shared_ptr<RifGenParams> RifGenParamsP; //给RifGenParams类型的指针取别名为RifGenParamsP
 
 
 struct RifGenerator {
